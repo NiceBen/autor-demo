5084902ef5882645bcc1e0666db2f97b77848643
OfBen <jpzhou2017@126.com>
2021-08-16 15:10:11 +0800

    first init

---
+++ src/main/java/com/ofben/autordemo/AutorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ofben.autordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutorDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ofben/autordemo/AutorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofben.autordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
