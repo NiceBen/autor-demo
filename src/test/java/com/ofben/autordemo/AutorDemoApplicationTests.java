c32d518dd003e811c6bbb60ca786ee76c3c1db16
OfBen <jpzhou2017@126.com>
2021-08-31 13:44:12 +0800

    add some test files

--- src/test/java/com/ofben/autordemo/AutorDemoApplicationTests.java
+++ src/test/java/com/ofben/autordemo/AutorDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ofben.autordemo;
 
+import com.ofben.autordemo.optimize.usertest.UserModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,41 @@ class AutorDemoApplicationTests {
     void contextLoads() {
     }
 
+    public static void main(String[] args) {
+
+//        complexTypeTransmit();
+
+        simpleTypeTransmit();
+    }
+
+    private static void simpleTypeTransmit() {
+        String str = "hello";
+        int intArg = 12;
+
+        operationArgs(str, intArg);
+
+        System.out.println("source str = " + str);
+        System.out.println("source intArg = " + intArg);
+    }
+
+    private static void operationArgs(String str, int intArg) {
+        System.out.println("target str = " + str);
+        System.out.println("target intArg = " + intArg);
+        System.out.println("------------");
+        str = "world";
+        intArg = 21;
+    }
+
+    private static void complexTypeTransmit() {
+        UserModel user = new UserModel();
+        user.setName("hello");
+        operationUser(user);
+        System.out.println("user.age=" + user.getAge());
+    }
+
+    private static void operationUser(UserModel user) {
+        System.out.println("operationUser内：" + user.getName());
+        System.out.println("operationUser内：" + user.getAge());
+        user.setAge(22);
+    }
 }
