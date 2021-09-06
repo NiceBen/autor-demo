99b074d1546ff834b96ae245a0c3074f19289b6b
OfBen <jpzhou2017@126.com>
2021-09-06 16:57:25 +0800

    feat(test):add multi Thread test

--- src/main/java/com/ofben/autordemo/test/poker/mock/MainTest.java
+++ src/main/java/com/ofben/autordemo/test/poker/mock/MainTest.java
@@ -9,7 +9,29 @@ package com.ofben.autordemo.test.poker.mock;
 public class MainTest {
 
     public static void main(String[] args) {
+        // 单线程
+        testSimpleThread();
+        // 多线程
+        testMultiThead();
+    }
+
+    private static void testSimpleThread() {
         PlayerUtil playerUtil = new PlayerUtil();
         playerUtil.beginGame();
+        System.out.println("-----");
+        playerUtil.beginGame();
+    }
+
+    private static void testMultiThead() {
+        try {
+            Thread.sleep(10000L);
+            for (int i = 0; i < 10000; i++) {
+                PlayerUtil playerUtil = new PlayerUtil();
+                playerUtil.beginGame();
+                System.out.println("-----");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
